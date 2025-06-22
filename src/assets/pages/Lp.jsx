990c2905816c64ecc60abe11758d829ba7d010ca
Justin Etienne <juetienne7@gmail.com>
2025-06-22 12:05:54 +0900

    sandbox section main div centered

--- src/assets/pages/Lp.jsx
+++ src/assets/pages/Lp.jsx
@@ -13,12 +13,16 @@ export default function Lp() {
           </span>
         </h1>
       </div>
-      <div className="w-[110%] -translate-[5%] h-96 bg-beige rotate-5 mt-48"></div>
-      <div className="-mt-[250px] z-10 relative font-dmsans font-medium text-3xl text-center">
+      <div className="relative w-[110%] left-1/2 -translate-x-1/2 h-[800px] bg-beige rotate-5 mt-48 mb-12">
+        <div className="relative left-1/2 -translate-x-1/2 -rotate-5 z-10  font-dmsans font-medium text-3xl text-center mb-48">
+          <h2 className="mb-12">Explore my sandbox of 3D micro-experiences.</h2>
+          <div className="w-[90%] sm:w-[80%] h-58 sm:h-72 md:h-96 bg-black mx-auto rounded-full"></div>
+        </div>
+      </div>
+      {/* <div className="-mt-[700px] z-10 relative font-dmsans font-medium text-3xl text-center mb-48">
         <h2 className="mb-12">Explore my sandbox of 3D micro-experiences.</h2>
         <div className="w-[90%] sm:w-[80%] h-58 sm:h-72 md:h-96 bg-black mx-auto rounded-full"></div>
-        {/* <div className="w-[80%] h-52 bg-black mx-auto rounded-full"></div> */}
-      </div>
+      </div> */}
     </>
   );
 }
